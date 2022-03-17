f056ccc587abf5bd009ab019945ca0a0d6495f70
Allaye <kolade199@gmail.com>
2022-03-17 03:13:16 +0100

    updates cli feature

--- directize/__init__.py
+++ directize/__init__.py
@@ -3,7 +3,7 @@ from config import directory, description, version
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="directize", description='CLI for directize package usage')
-    parser.add_argument('-d', '--directory', type=str, choices=directory.keys(), help='choose a directory type to create', required=True)
-    parser.add_argument('-d', '--directory', help='directory to directize', required=True)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + version)
+    parser.add_argument('-d', '--directory', type=str, choices=directory.keys(), help='choose a directory type to create')
     args = parser.parse_args()
     print(args)
