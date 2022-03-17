d8d74c38d66cb7e3916f011d82c79dafbff5a17a
Allaye <kolade199@gmail.com>
2022-03-17 22:01:27 +0100

    improved and hookup standard module to cli

--- directize/directize.py
+++ directize/directize.py
@@ -1,9 +1,12 @@
 import argparse
 from config import directory, description, version
+from standard import standardproject
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="directize", description='CLI for directize package usage')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + version)
-    parser.add_argument('-d', '--directory', type=str, choices=directory.keys(), help='choose a directory type to create')
+    parser.add_argument('-d', '--directory', type=str, choices=directory.keys(), help='choose a directory structure to create', required=True)
+    parser.add_argument('-p', '--project', type=str, help='name of the project been created', required=False)
     args = parser.parse_args()
-    print(args)
+    print(args.project)
+    standardproject(args.project)
